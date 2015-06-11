d908c80e33571659fe85a39da4a23cd652d243dd
Jorge Sanz <jorsanzp@gmail.com>
2015-06-11 20:30:46 +0200

    Correct syntax

--- Tutorial/Starting/IAmAnIntensionalList.hs
+++ Tutorial/Starting/IAmAnIntensionalList.hs
@@ -1,3 +1,3 @@
 boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 length' xs = sum [1 | _ <- xs]
-removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
